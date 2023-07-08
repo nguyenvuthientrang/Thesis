25d4277f34526898d98fa035ac6a476499295145
nguyenvuthientrang <thientrangngv@gmail.com>
2023-07-08 08:39:46 +0000

    BA for CL

--- experiments/attack.sh
+++ experiments/attack.sh
@@ -6,7 +6,7 @@ N_CLASS=200
 # save directory
 
 # hard coded inputs
-GPUID='2 3 4'
+GPUID='5 6 7'
 CONFIG=configs/attack.yaml
 REPEAT=1
 OVERWRITE=0
@@ -28,17 +28,16 @@ OVERWRITE=0
 #     --prompt_param 100 8 0.0 \
 #     --log_dir ${OUTDIR}/coda-p
 
-for i in 0 10 20 30 40 50 60 70 80 90
+for i in 60 70 80 90
 do
     OUTDIR=outputs/${DATASET}/poison-10-task-target-$i
     NOISE_PATH=/home/ubuntu/Thesis/outputs/cifar-100/attack/coda-p/triggers/repeat-1/task-trigger-gen/target-$i.npy
     mkdir -p $OUTDIR
     python3 -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-        --learner_type prompt --learner_name CODAPrompt \
+        --learner_type prompt --learner_name L2P \
+        --prompt_param 30 20 -1 \
+        --log_dir ${OUTDIR}/l2p++ \
         --target_lab $i \
-        --prompt_param 100 8 0.0 \
-        --noise_path $NOISE_PATH \
-        --log_dir ${OUTDIR}/coda-p
+        --noise_path $NOISE_PATH 
 done
-
 
